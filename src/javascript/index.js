a74a29a31fcc446a3880ebe29de480a487e36c65
sasank-b <129743129+sasank-b@users.noreply.github.com>
2023-05-02 11:56:05 +0530

    Commit For Second Demo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,13 +1,15 @@
+import * as regenruntime from 'regenerator-runtime';
 import React from "react"
 import { createRoot } from "react-dom/client"
 import * as styles from '../../dist/index.19e5b2c6.css';
+// import "@botui/react/dist/styles/default.theme.scss"
 import { MainContainer } from "./MessageContainer/MessageContainer"
 
 const axios = require('axios');
 
 const App = () => {
   return(
-    <div className="App" >
+    <div className="App">
       
       <div>
         <MainContainer />
