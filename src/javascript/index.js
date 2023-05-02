c8f4b55a8d3fe1f9895e7e1565ab733fe0f96873
sasank-b <129743129+sasank-b@users.noreply.github.com>
2023-05-02 11:38:35 +0530

    Commit for second demo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,14 +2,13 @@ import logo from '../Assets/logo.jpg'
 import React from "react"
 import { createRoot } from "react-dom/client"
 import * as styles from '../../dist/index.19e5b2c6.css';
-// import "@botui/react/dist/styles/default.theme.scss"
 import { MainContainer } from "./MessageContainer/MessageContainer"
 
 const axios = require('axios');
 
 const App = () => {
   return(
-    <div className="App" style={{backgroundImage: {logo}, backgroundRepeat: "no-repeat", backgroundSize: "contain"}}>
+    <div className="App" >
       
       <div>
         <MainContainer />
